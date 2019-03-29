f57d4c0797455a308050189e825d70901a092ebe
KyleDeflerUK <kdde223@kdde223.netlab.uky.edu>
2019-03-29 11:19:40 -0400

    test file for q4

---
+++ idFromFilenameTestfile.java
@@ -0,0 +1,24 @@
+mport org.junit.Test;
+
+public class idFromFilenameTest {
+
+
+    @Test //test 1
+    public void test1()
+    {
+        MyStack<String> stack = new MyStack<String>();
+	if (filename.length != 0){
+		assertTrue(stack.empty());
+	}
+    }
+
+    @Test //test 2
+    public void test2()
+    {
+        MyStack<String> stack = new MyStack<String>();
+	if(filename.length > 0){
+	        assertTrue(stack.empty());
+	}
+    }
+    
+} 
